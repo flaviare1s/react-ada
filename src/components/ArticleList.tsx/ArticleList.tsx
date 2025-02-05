561c782d6957574b1ce0baad919bbe4f43dce281
flaviare1s <flaviarster@gmail.com>
2025-02-05 14:12:16 -0300

    Integra o site com a API Space News

--- src/components/ArticleList.tsx/ArticleList.tsx
+++ src/components/ArticleList.tsx/ArticleList.tsx
@@ -1,33 +1,36 @@
 import "./styles.css";
-import React from "react";
 import { Article } from "../Article/Article";
-import article1Img from "../../assets/images/article1.png";
-import article2Img from "../../assets/images/article2.png";
-import article3Img from "../../assets/images/article3.png";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export class ArticleList extends React.Component {
-  render() {
+export const ArticleList = () => {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    async function loadNews() {
+      const response = await axios.get(
+        "https://api.spaceflightnewsapi.net/v4/articles/"
+      );
+      const newsData = response.data;
+      console.log(newsData);
+      setNews(newsData.results);
+    }
+    loadNews();
+  }, []);
+ 
     return (
       <section id="articles">
-        <Article
-          title="Designing Dashboards"
-          provider="NASA"
-          description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-          image={article1Img}
-        />
-        <Article
-          title="Vibrant Portraits of 2020"
-          provider="SpaceNews"
-          description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-          image={article2Img}
-        />
-        <Article
-          title="36 Days of Malayalam type"
-          provider="Spaceflight Now"
-          description="Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet."
-          image={article3Img}
-        />
+        { news.map((article) => {
+          return (
+          <Article
+          key={article.id}
+            title={article.title}
+            provider={article.news_site}
+            description={article.summary}
+            image={article.image_url}
+          />
+          )
+        })}
       </section>
     );
   }
-}
